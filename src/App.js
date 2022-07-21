e8a7da198241688d14868e06d6eb069d5e37aaca
gaoyurt112 <1015126456@qq.com>
2022-07-21 17:47:27 +0800

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.scss'
+import { Select } from '@douyinfe/semi-ui'
+function App () {
+  const list = [
+    { value: 'abc', label: '管理处一', otherKey: 0 },
+    { value: 'abc', label: '管理处二', otherKey: 1 },
+    { value: 'abc', label: '管理处三', otherKey: 2 },
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>实时就餐人数展示</div>
+      <div className='main-top'>
+        {/* 昨日数据看板 */}
+        <div className='recent-data'></div>
+        {/* 用餐人数 */}
+        <div className='pe-number'>
+          <div className='number'>200</div>
+          <div className='ren'>人</div>
+        </div>
+        {/* 管理处选择 */}
+        <div className='selectForm'>
+          <div className='select-title'>管理处：</div>
+          <div><Select placeholder='请选择管理处' style={{ width: 300 }} optionList={list}>
+          </Select></div>
+
+        </div>
+        {/* 餐厅状态 */}
+        <div className='pe-status'></div>
+      </div>
+
+      <div className='main-bottom'>
+
+      </div>
+
+
+
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
